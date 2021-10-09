50254178fd2a6e4cb261cf5c79c9c0799113ee59
Surojiddin <isurojiddin@gmail.com>
2021-10-09 20:51:03 +0500

    Task

--- src/main/scala/WordsProgramSurojiddin.scala
+++ src/main/scala/WordsProgramSurojiddin.scala
@@ -1,29 +1,68 @@
 import scala.io.StdIn.readLine
 
-object WordsProgramSurojiddin extends App{
+object WordsProgramSurojiddin extends App {
 
-  var langs = List("Scala", "Java", "PHP", "JavaScript", "PostgreSQL")
+  case class Word(value: String, definition: String)
+
+  val wordsWithDefinition = List(
+    Word("val", "Val bu constanta"),
+    Word("var", "Var bu o'zgaruvchi")
+  )
+  val words = List("val", "var", "map", "/", "%", "for", "foreach", "function", "method", "list", "readLine", "readInt", "readDouble", ":+", "+:", ":::", "::")
   val coders = List("Surojiddin", "Navruz", "Og'abek")
+  var knBase = Map.empty[String, List[String]]
 
-  for (i <- coders){
-    var randomLangs = scala.util.Random.nextInt(langs.length)
-    println(s"$i - ${langs(randomLangs)}")
-    langs = langs.filterNot(_ == langs(randomLangs))
+  val k = words.length / coders.length
+
+  def insertWord(name: String, word: String): Unit =
+    knBase += knBase.get(name).map { words =>
+      name -> (word +: words)
+    }.getOrElse(name -> List(word))
+
+  for {
+    _ <- 0 until k
+    name <- coders
+  } {
+    def selectedWord: String = words(scala.util.Random.nextInt(words.length))
+
+    def recursiveTask(newWord: String): String =
+      knBase.get(name).map { coderWords =>
+        if (coderWords.contains(newWord)) {
+          recursiveTask(selectedWord)
+        } else {
+          insertWord(name, newWord)
+          newWord
+        }
+      }.getOrElse(newWord)
+
+    println(s"$name - ${recursiveTask(selectedWord)}")
+
+    readLine()
   }
 
-  val str = readLine()
-  val list2 = langs :+ str
-  println(list2)
+  //  @tailrec
+  //  def rec(list: List[Int]): Unit = list match {
+  //    case head :: tail =>
+  //      println("element" + head.toString)
+  //      rec(tail)
+  //    case Nil => println("Tugadi")
+  //  }
+  //
+  //  rec(1 :: 2:: 3 :: 4 ::Nil)
 
-  val str2 = readLine()
-  var fruits = List("Apple", "Banana")
+  //  println(knBase)
 
-  if (str2 == fruits.filter(_ == str2).toString){
-    println("Olma")
-  } else {
-    println("Bunday so'z topilmadi")
-  }
+  //  val str = readLine()
+  //  val list2 = langs :+ str
+  //  println(list2)
 
-
+  //  val str2 = readLine()
+  //  var fruits = List("Apple", "Banana")
+  //
+  //  if (str2 == fruits.filter(_ == str2).toString){
+  //    println("Olma")
+  //  } else {
+  //    println("Bunday so'z topilmadi")
+  //  }
 
 }
